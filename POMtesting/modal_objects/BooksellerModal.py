f18747f3abe5648cea729d20a431b8c7a09c1ead
Emma <emma@zolabooks.com>
2013-07-10 17:50:24 -0400

    bookseller profile and modal added

---
+++ POMtesting/modal_objects/BooksellerModal.py
@@ -0,0 +1,30 @@
+'''
+Created on Jul 10, 2013
+
+@author: emma
+'''
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+from robot.libraries.BuiltIn import BuiltIn
+
+class bookseller_modal():
+    def __init__(self, webd_wrap):
+        self._webd_wrap = BuiltIn().get_library_instance('WebDriverWrapper')
+    
+    def detect_modal_element(self):
+        self._webd_wrap._driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div/div/div/div/section[2]/div/section[2]/div/div[2]/div/div/a")
+        return self
+   
+    def modal_should_be_present(self):
+        ''' raises AssertionError if modal is not displayed '''
+        self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, "l-570px")))
+        return self
+    
+    def click_full_profile(self):
+        full_profile = self._driver.find_element_by_class_name('name-header').find_element_by_xpath('div/a')
+        self._webd_wrap._driver.execute_script("(arguments[0]).click()", full_profile)
+        
---
+++ POMtesting/page_objects/BooksellerProfile.py
@@ -0,0 +1,29 @@
+'''
+Created on Jul 10, 2013
+
+@author: emma
+'''
+
+from selenium import webdriver #imports selenium
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+from selenium.webdriver.common.action_chains import ActionChains
+from robot.libraries.BuiltIn import BuiltIn
+
+import time
+
+class bookseller_profile():
+    
+    def __init__(self, webd_wrap):
+        self._webd_wrap = BuiltIn().get_library_instance('WebDriverWrapper')
+        
+    def click_pledge(self):
+        self._webd_wrap._driver.find_element_by_xpath('/html/body/div[3]/div/div/section/p/a/img').click()    
+        
+    def click_unpledge(self):
+        self._webd_wrap._driver.find_element_by_xpath('/html/body/div[3]/div/div/section/div/a').click()
+    
+    def click_my_zola(self):
+        time.sleep(2)
+        self._webd_wrap._driver.find_element_by_id('h-user-personalized-toolbar').find_element_by_xpath('div/a').click()
