7afbcd5373b6b2da3678ccea6a7f624f5ca8bde0
Emma <emma@zolabooks.com>
2013-07-10 11:20:03 -0400

    added recommend functions

--- POMtesting/modal_objects/RecommendModal.py
+++ POMtesting/modal_objects/RecommendModal.py
@@ -19,16 +19,21 @@ class RecommendModal():
      
     def submit_recommend(self):
         self.enter_email()
+        self.click_checkbox()
         self.enter_message()
         self.submit() 
        
     def enter_email(self):
         _recommend_email_form = self._webd_wrap._driver.find_element_by_id('recommend-modal').find_element_by_id('recommend-email-form')
         self._webd_wrap._driver.execute_script('$(arguments[0]).val(arguments[1])', _recommend_email_form.find_elements_by_class_name('textboxlist-bit-editable-input')[0], '1@zolabooks.com')
-        
-    def enter_message(self):
-        checkbox = self._webd_wrap._driver.find_element_by_id('uniform-share-with-followers').find_element_by_xpath('span/input')
+    
+    def click_checkbox(self):
+        checkbox = self._driver.find_element_by_id('uniform-share-with-followers').find_element_by_xpath('span/input')
         self._webd_wrap._driver.execute_script("$(arguments[0]).click()", checkbox)
+            
+    def enter_message(self):
+        _recommend_email_form = self._webd_wrap._driver.find_element_by_id('recommend-email-form')
+        self._driver.execute_script('$(arguments[0]).val(arguments[1])', _recommend_email_form.find_elements_by_name('message')[0], 'test')
         
     def submit(self):
         send = self._webd_wrap._driver.find_element_by_id('recommend-modal').find_element_by_id("recommend-email-form").find_element_by_xpath("footer/input")
