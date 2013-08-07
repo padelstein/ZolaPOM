2d58f21ced0b65f595d9d4df28ddf418b00d6d87
Patrick Adelstein <padelstein@middlebury.edu>
2013-08-07 13:10:27 -0400

    added random name functionality

--- POMtesting/page_objects/WebDriverWrapper.py
+++ POMtesting/page_objects/WebDriverWrapper.py
@@ -21,8 +21,11 @@ class WebDriverWrapper:
 #         self._authURL = 'https://zola_stage:zola123@cultess.divergence.zolaqc.com'
 #         self._baseURL = 'https://cultess.divergence.zolaqc.com'
 
-        self._authURL = 'https://zola_stage:zola123@zolaqc.com'
-        self._baseURL = 'https://zolaqc.com'
+#         self._authURL = 'https://zola_stage:zola123@zolaqc.com'
+#         self._baseURL = 'https://zolaqc.com'
+
+        self._authURL = 'https://zolabooks.com'
+        self._baseURL = 'https://zolabooks.com'
 
 
 
